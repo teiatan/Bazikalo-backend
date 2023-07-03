53b4fee5ecd5ed7a527bb9cf31a7e86ae61e6347
teiatan <92535482+teiatan@users.noreply.github.com>
2023-07-04 01:53:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static("public"));
 
+app.all('*', function(req, res, next) {
+    const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.post('/auth', async (req, res) => {
     const {userName} = req.body;
     const createdUser = await User.create({userName});
