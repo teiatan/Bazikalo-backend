78933dff92ad9f526c64cc5d9df63bfbae895159
teiatan <92535482+teiatan@users.noreply.github.com>
2023-07-04 01:43:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://127.0.0.1:5173',
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
