7637373c4def9624528cf197196ff4e03bd9aeec
teiatan <92535482+teiatan@users.noreply.github.com>
2023-07-06 14:17:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'https://bazikalo.vercel.app/',
+    origin: 'http://127.0.0.1:5173',
   }));
 app.use(express.json());
 app.use(express.static("public"));
